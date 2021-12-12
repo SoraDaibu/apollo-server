4405596eeb1293b665502f4b1695824ac5b09d41
sora_shrlock <soradaibu141221@gmail.com>
2021-12-12 18:52:50 +0900

    ♻️ generates types for TS

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import {ApolloServer} from 'apollo-server';
+import {ApolloServer, AuthenticationError} from 'apollo-server';
 import {loadSchemaSync} from '@graphql-tools/load';
 import {GraphQLFileLoader} from '@graphql-tools/graphql-file-loader';
 import {addResolversToSchema} from '@graphql-tools/schema';
 import {join} from 'path';
+import {Resolvers} from './types/generated/graphql';
+import {Context} from './types/context';
 
 const books = [
   {
@@ -22,14 +24,40 @@ const schema = loadSchemaSync(join(__dirname, '../schema.graphql'), {
 // リゾルバーの定義
 const resolvers = {
   Query: {
-    books: () => books,
+    books: () => {
+      // todo: proceed something here
+      return books;
+    },
   },
 };
 
 const schemaWithResolvers = addResolversToSchema({schema, resolvers});
 
+const getUser = (token?: string): Context['user'] => {
+  if (token === undefined) {
+    throw new AuthenticationError(
+      'Unautherized user cannot access to the resources.'
+    );
+  }
+
+  // todo: Tokenからユーザ情報を取り出す処理
+
+  return {
+    name: 'dummy name',
+    email: 'dummy@example.com',
+    token,
+  };
+};
+
 // サーバーの起動
-const server = new ApolloServer({schema: schemaWithResolvers});
+const server = new ApolloServer({
+  schema: schemaWithResolvers,
+  context: ({req}) =>
+    ({
+      user: getUser(req.headers.authorization),
+    } as Context),
+  debug: false, // set 'false' for prod
+});
 
 server.listen().then(({url}) => {
   console.log(`🚀  Server ready at ${url}`);
